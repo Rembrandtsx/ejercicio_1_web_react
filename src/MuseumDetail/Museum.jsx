5114bdeaa483d92292ccdc87c3cfdb8eaf3f40c7
Camilo Salinas <iam@camilosalinas.me>
2022-04-01 13:02:44 -0500

    Starter template

---
+++ src/MuseumDetail/Museum.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Museum() {
+  return <div>Museum</div>;
+}
+
+export default Museum;
---
+++ src/MuseumList/MuseumsList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MuseumsList() {
+  return <div>MuseumsList</div>;
+}
+
+export default MuseumsList;
